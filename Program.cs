4a8ab95112ffe00ada0099957c16ac7cd8049954
mariya-12 <mariya.polson@rapidvaluesolutions.com>
2021-09-08 13:33:11 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeDetails employeedetails = new EmployeeDetails();
+            employeedetails.getvalidateDetailsandPrintDetails();
+        }
+    }
+}
